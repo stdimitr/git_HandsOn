02f7a437a39db7856fc5818696897ef9bcd0f7d0
STAVROS DIMITRIADIS <stidimitriadis@gmail.com>
2024-02-25 23:09:26 +0200

    Update seqClass.py

--- seqClass.py
+++ seqClass.py
@@ -21,8 +21,8 @@ if re.search('^[ACGTU]+$', args.seq):
     elif re.search('U', args.seq) and not re.search('T', args.seq): #Only U is present
         print ('The sequence is RNA')
     elif re.search('T', args.seq) and re.search('U', args.seq): # For a wrong typing where both T & U are present !
-        print ('ERROR: The sequence contains both T and U')
-    else: # We cannot decide if it is DNA or RNA - both are possible - If the sequence contains ONLY AG
+        print ('The sequence cannot contain both T and U')
+    else: # If the sequence contains ONLY AG than we cannot decide if it is DNA or RNA sequence
         print ('The sequence can be DNA or RNA')
 else: # neither DNA or RNA
     print ('The sequence is neither DNA nor RNA')
