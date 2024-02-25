4b020b9346cc85ec83704c83b6cbc7482de6fd83
STAVROS DIMITRIADIS <stidimitriadis@gmail.com>
2024-02-25 23:15:58 +0200

    Update percentage_nucleotide.py

--- percentage_nucleotide.py
+++ percentage_nucleotide.py
@@ -17,11 +17,15 @@ args = parser.parse_args()
 args.seq = args.seq.upper()
 
 if re.search('^[ACGTU]+$', args.seq):
-    if re.search('U', args.seq):
-        print ('The sequence is RNA')
-    else:
+    if re.search('T', args.seq) and not re.search('U', args.seq): #Only T is present (NOT U is present)
         print ('The sequence is DNA')
-else:
+    elif re.search('U', args.seq) and not re.search('T', args.seq): #Only U is present (NOT T is present)
+        print ('The sequence is RNA')
+    elif re.search('T', args.seq) and re.search('U', args.seq): # If we type a wrong sequence that includes both T & U
+        print ('The sequence cannot contain both T and U')
+    else: # If the sequence contains ONLY ACG than we cannot decide if it is DNA or RNA sequence
+        print ('The sequence can be DNA or RNA')
+else: # neither DNA or RNA
     print ('The sequence is neither DNA nor RNA')
     sys.exit(1) 
 
